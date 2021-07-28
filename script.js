850de0df01bc5e80a102878e2e23ff3118a79a9a
Daniel Song <danieljsong15@gmail.com>
2021-07-27 20:56:53 -0700

    Added emoji options choices, created CSS file. Reworked some of the JS code

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-let playerScore = 0;
-let computerScore = 0;
 const resultPanel = document.querySelector('#results');
+const scorePanel = document.querySelector
 const playerResult = document.createElement('p');
 const computerResult = document.createElement('p');
 const playerChoice = document.querySelectorAll('.choice');
+let playerScore = 0;
+let computerScore = 0;
 
 function computerPlay() {
     let choices = ['Rock', 'Paper', 'Scissors']
@@ -17,24 +18,20 @@ function playRound(player, computer) {
     computerResult.textContent = `The computer chose ${computer}`;
     resultPanel.appendChild(playerResult);
     resultPanel.appendChild(computerResult);
-    //console.log(`The player chose ${player}`);
-    //console.log(`The computer chose ${computer}`);
 
     if (player === computer) {
         return ("It's a tie!");
-    } else if (player === "Rock" && computer === "Scissors"
-            || player === "Scissors" && computer === "Paper"
-            || player === "Paper" && computer === "Rock") {
+    } else if (player === "Rock" && computer === "Scissors" ||
+               player === "Scissors" && computer === "Paper" ||
+               player === "Paper" && computer === "Rock") {
         playerScore += 1;
         return (`You win! ${player} beats ${computer}`);
-    } else if (player === "Rock" && computer === "Paper"
-            || player === "Scissors" && computer === "Rock"
-            || player === "Paper" && computer === "Scissors") {
+    } else if (player === "Rock" && computer === "Paper" ||
+               player === "Scissors" && computer === "Rock" ||
+               player === "Paper" && computer === "Scissors") {
         computerScore += 1;
         return (`You lose! ${computer} beats ${player}`);
     }
-
-
 }
 
 function game() {
