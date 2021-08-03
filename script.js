00efee734676dbffaa01c5c40b11f05ee98d175a
Daniel Song <danieljsong15@gmail.com>
2021-08-03 15:43:42 -0700

    Changed winner announcement from alert to DOM appendix

--- script.js
+++ script.js
@@ -5,42 +5,50 @@ const playerScoreboard = document.querySelector('#playerScore');
 const computerScoreboard = document.querySelector('#computerScore');
 const score = document.querySelectorAll('.score');
 const message = document.querySelector('#game-message');
+const div = document.createElement('div');
+const appContainer = document.querySelector('#app');
 let playerScore = 0;
 let computerScore = 0;
 
-playerScoreboard.innerHTML = playerScore;
-computerScoreboard.innerHTML = computerScore;
+playerScoreboard.textContent = playerScore;
+computerScoreboard.textContent = computerScore;
 
 function computerPlay() {
     let choices = ['Rock', 'Paper', 'Scissors'];
-
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function playRound(player, computer) {
-    playerDisplay.innerHTML = player;
-    computerDisplay.innerHTML = computer;
+    playerDisplay.textContent = player;
+    computerDisplay.textContent = computer;
 
     if (player === computer) {
-        message.innerHTML = `It's a tie!`;
+        message.textContent = `It's a tie!`;
     } else if (player === "Rock" && computer === "Scissors" ||
                player === "Scissors" && computer === "Paper" ||
                player === "Paper" && computer === "Rock") {
         playerScore += 1;
-        message.innerHTML = `You win! ${player} beats ${computer}`;;
+        message.textContent = `You win! ${player} beats ${computer}`;
     } else if (player === "Rock" && computer === "Paper" ||
                player === "Scissors" && computer === "Rock" ||
                player === "Paper" && computer === "Scissors") {
         computerScore += 1;
-        message.innerHTML = `You lose! ${computer} beats ${player}`;;
+        message.textContent = `You lose! ${computer} beats ${player}`;
     }
 
-    if (playerScore == 5 && playerScore > computerScore) {
-        alert(`You beat the computer!!!`);
-    } else if (computerScore == 5 && computerScore > playerScore) {
-        alert(`The computer beat you!!!`);
+    playerScoreboard.textContent = playerScore;
+    computerScoreboard.textContent = computerScore;
+
+    if (playerScoreboard.textContent == 5 && playerScore > computerScore) {
+        div.textContent = `You beat the computer!!!`;
+        div.style.cssText = 'color: blue; font-weight: bold:; font-size: 25px; text-align: center';
+        appContainer.appendChild(div);
+    } else if (computerScoreboard.textContent == 5 && computerScore > playerScore) {
+        div.textContent = `The computer beat you!!!`;
+        div.style.cssText = 'color: red; font-weight: bold:; font-size: 25px; text-align: center';
+        appContainer.appendChild(div);
     }
-}
+};
 
 /*function game() {
     for (i = 1; i <= 5; i++) {
@@ -64,23 +72,18 @@ function playRound(player, computer) {
 }*/
 
 function reset() {
-    playerDisplay.innerHTML = '';
-    computerDisplay.innerHTML = '';
-    message.innerHTML = '';
+    playerDisplay.textContent = '';
+    computerDisplay.textContent = '';
+    message.textContent = '';
 
     playerScore = 0;
     computerScore = 0;
-    playerScoreboard.innerHTML = playerScore;
-    computerScoreboard.innerHTML = computerScore;
-}
+    playerScoreboard.textContent = playerScore;
+    computerScoreboard.textContent = computerScore;
+
+    appContainer.removeChild(div);
+};
 
 playerChoice.forEach(choice => choice.addEventListener('click', function (e) {
     playRound(e.composedPath()[0].value, computerPlay());
-
-    playerScoreboard.innerHTML = playerScore;
-    computerScoreboard.innerHTML = computerScore;
-
-
 }));
-
-score.forEach(value => value.addEventListener(''))
